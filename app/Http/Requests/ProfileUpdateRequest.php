d34f1727783b40b9ecec5a7bc965eb0a8c27a5fb
ayanakkabi1 <aynour200609@gmail.com>
2026-02-24 17:52:23 +0000

    feat: add validation rules for pseudo and photo fields in profile update request

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,8 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'pseudo' => ['required', 'string', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'photo'  =>['nullable','image','mimes:jpg,jpeg,png','max:2048'],
         ];
     }
 }
