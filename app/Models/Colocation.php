4ca64eab6c6b63962e8b99726f5554be00ebe711
ayanakkabi1 <aynour200609@gmail.com>
2026-02-24 12:27:58 +0000

    feat: add Colocation model with relationships

---
+++ app/Models/Colocation.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+class Colocation extends Model
+{
+    protected $fillable = ['name','statut','owner_id'];
+    public function owner(){
+        return $this->belongsTo(User::class,'owner_id');
+    }
+    public function members()
+    {
+        return $this->belongsToMany(User::class)
+                    ->withPivot('role', 'joined_at')
+                    ->withTimestamps();
+    }
+}
+?>
