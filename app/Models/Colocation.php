ceb084533dc1c09cd33f3f3450cb7a6282aedb40
ayanakkabi1 <aynour200609@gmail.com>
2026-02-26 14:22:55 +0000

    Add routes for colocation creation and storage
    - Introduced a new route for displaying the colocation creation view.
    - Added middleware protection for colocation creation and storage routes.
    - Integrated ColocationController for handling colocation-related requests.

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 class Colocation extends Model
 {
     protected $fillable = ['name','statut','owner_id'];
+    
     public function owner(){
         return $this->belongsTo(User::class,'owner_id');
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ColocationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/create', function () {
+    return view('colocation.create');
+});
+Route::middleware('auth')->group(function () {
+    Route::get('/create', [ColocationController::class, 'CreateColocation'])->name('colocation.create');
+    Route::post('/colocation/store', [ColocationController::class, 'storeColocation'])->name('Colocation.store');
+});
 require __DIR__.'/auth.php';
