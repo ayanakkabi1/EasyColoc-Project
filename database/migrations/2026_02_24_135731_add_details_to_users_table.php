1b41d3f516fc7835c51efc7a0d23e1b4513b8581
ayanakkabi1 <aynour200609@gmail.com>
2026-02-26 12:25:49 +0000

    feat: make pseudo field nullable in users table migration

--- database/migrations/2026_02_24_135731_add_details_to_users_table.php
+++ database/migrations/2026_02_24_135731_add_details_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('pseudo')->unique()->after('name');
+            $table->string('pseudo')->nullable()->unique()->after('name');
             $table->string('photo')->nullable()->after('password');
         });
     }
