e40092ebc643230bbc99e8c37a36ddd9ae5dd894
ayanakkabi1 <aynour200609@gmail.com>
2026-03-03 09:40:39 +0000

    fix: correct column name from 'status' to 'statut' in activecolocation method

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable
     
     public function activecolocation(){
         return $this->colocations()
-            ->where('colocations.status', 'active')
+            ->where('colocations.statut', 'active')
             ->with('users')
             ->first();
     }
